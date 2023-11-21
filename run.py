210185371bd5f8e46446cce2282bcff6e692a88b
dvkostousov <dancool55555@gmail.com>
2023-11-21 21:47:32 +0300

    files fix

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from app import create_app
 
-app = create_app()
+application = create_app()
 
 if __name__ == '__main__':
-    app.run(port="8001", debug=False)
+    application.run(port="8001", debug=False)
